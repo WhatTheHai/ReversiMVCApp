c886e1b65c00ae54d9a82626e2af9a704234e6e4
WhatTheHai <haiboy@hotmail.nl>
2023-06-29 14:33:43 +0200

    test for now

--- ReversiMvcApp/Startup.cs
+++ ReversiMvcApp/Startup.cs
@@ -27,9 +27,9 @@ namespace ReversiMvcApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ReversiDbContext>(options => 
+            services.AddDbContext<ReversiDbContext>(options =>
             {
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+                options.UseSqlite("Data Source=application.db;Cache=Shared;Mode=ReadWrite");
             });
             services.AddDbContext<ApplicationDbContext>(options => 
             {
