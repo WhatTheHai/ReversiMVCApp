5c3c22eaa4112235f25b08f58539387760e8313b
WhatTheHai <haiboy@hotmail.nl>
2023-07-22 00:13:56 +0200

    Security update

--- ReversiMvcApp/Startup.cs
+++ ReversiMvcApp/Startup.cs
@@ -64,6 +64,13 @@ namespace ReversiMvcApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                await next();
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
